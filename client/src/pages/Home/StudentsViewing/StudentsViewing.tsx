d6f33a30f52fe0bd92d6281b2ba749c73870849f
khudnytskaya <54635860+khudnytskaya@users.noreply.github.com>
2020-04-29 14:19:49 +0300

    changes

--- client/src/pages/Home/StudentsViewing/StudentsViewing.tsx
+++ client/src/pages/Home/StudentsViewing/StudentsViewing.tsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
-import classes from './Lections.module.css';
-import CardItem from "./CardItem";
+import classes from './StudentsViewing.module.css';
+import CardItem from '../RecomendedLections/Lections/CardItem'
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import {connect, ConnectedProps} from "react-redux";
-import {getLectures} from "../../../../store/actions/getLectures";
-import {RootState} from "../../../../store";
+import {getLectures} from "../../../store/actions/getLectures";
+import {RootState} from "../../../store";
+
 
 const mapStateToProps = (state: RootState) => ({
   lecturesList: state.lectures.lectures
@@ -21,48 +22,44 @@ const mapDispatchToProps = (dispatch: Function) => ({
 const connector = connect(mapStateToProps, mapDispatchToProps);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
-
-const Lections = ({lecturesList, getLectures}: PropsFromRedux) => {
+const StudentsViewing = ({lecturesList, getLectures}: PropsFromRedux) => {
   // const [lection, setLection] = useState([]);
-  
-  // const updateLecture = () => {
-  //   setLection({ lecturesList});
-  // }
-
-  // const updateLecture = () => setLection(getLectures());
 
   // useEffect(() => {
-  //   getLectures();
-  //   // updateLecture()
+  //   getLectures()
   // }, [])
+
 
   const renderLectures = (arr:any) => {
     console.log(arr)
     return arr.map((item:any, index:any) => {
+
       return (
         <CardItem item={item} />
       )
     })
 
   }
+
   const lectionCard = renderLectures(lecturesList);
 
   const settings = {
     dots: false,
     infinite: false,
     speed: 900,
-    slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToShow: 4,
+    slidesToScroll: 4
   };
 
   return (
-    <div className={classes.cardsFlex}>
-      <Slider {...settings} >
-      {lectionCard}
+    <div className={classes.wrapper}>
+      <p className={classes.textStudent}>Students are viewing</p>
+
+      <Slider {...settings}>
+        {lectionCard}
       </Slider>
     </div>
   )
 }
 
-export default connector(Lections);
-
+export default connector(StudentsViewing);
