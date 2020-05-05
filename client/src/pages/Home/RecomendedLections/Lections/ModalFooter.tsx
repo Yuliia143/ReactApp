35ffceb800388861bdb82f25ea3a494a2e96a492
khudnytskaya <54635860+khudnytskaya@users.noreply.github.com>
2020-05-05 10:58:33 +0300

    changes

--- client/src/pages/Home/RecomendedLections/Lections/ModalFooter.tsx
+++ client/src/pages/Home/RecomendedLections/Lections/ModalFooter.tsx
@@ -5,56 +5,55 @@ import { Link } from 'react-router-dom';
 import { RootState } from '../../../../store';
 import http from '../../../../api/http';
 
-
 const ModalFooter = ({ id, onClose, userData }: any) => {
 
-  const favoriteLection = async(id : any) =>{
-    const responce = await http.put(`/api/lectures/${id}/add_to_favs`, {})
-    return responce.data;
-  }  
+  // const favoriteLection = async(id : any) =>{
+  //   const responce = await http.put(`/api/lectures/${id}/add_to_favs`, {})
+  //   return responce.data;
+  // }  
 
   return (
-    <Modal.Actions>
-      <Button color='black' onClick={onClose} >
-        Cancel
-              </Button>
-      {userData !== null ? <Button
-        positive
-        icon='heart outline'
-        labelPosition='right'
-        content="Add to favorite"
-        onClick={() => favoriteLection(id)}
-      /> : <Link to='/signin'><Button
-        positive
-        icon='checkmark'
-        labelPosition='right'
-        content="Go to signin"
-        onClick={onClose}
-      /></Link>}
-
-    </Modal.Actions>
-
-
     // <Modal.Actions>
-
     //   <Button color='black' onClick={onClose} >
     //     Cancel
-    //   </Button>
-    //   {userData!== null ? <Link to={`/lecture/${id}`}><Button
+    //           </Button>
+    //   {userData !== null ? <Button
     //     positive
     //     icon='heart outline'
     //     labelPosition='right'
     //     content="Add to favorite"
+    //     onClick={() => favoriteLection(id)}
+    //   /> : <Link to='/signin'><Button
+    //     positive
+    //     icon='checkmark'
+    //     labelPosition='right'
+    //     content="Go to signin"
     //     onClick={onClose}
-    //   /></Link> : <Link to='/signin'><Button
-    //   positive
-    //   icon='checkmark'
-    //   labelPosition='right'
-    //   content="Go to signin"
-    //   onClick={onClose}
-    // /></Link>}
+    //   /></Link>}
 
     // </Modal.Actions>
+
+
+    <Modal.Actions>
+
+      <Button color='black' onClick={onClose} >
+        Cancel
+      </Button>
+      {userData!== null ? <Link to={`/lecture/${id}`}><Button
+        positive
+        icon='heart outline'
+        labelPosition='right'
+        content="Add to favorite"
+        onClick={onClose}
+      /></Link> : <Link to='/signin'><Button
+      positive
+      icon='checkmark'
+      labelPosition='right'
+      content="Go to signin"
+      onClick={onClose}
+    /></Link>}
+
+    </Modal.Actions>
 
   )
 }
