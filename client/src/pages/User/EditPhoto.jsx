786002841f4629c903bacf8f12b754300ba1cd99
Ihor Dubas <ihordubas1525@gmail.com>
2020-05-08 18:44:11 +0300

    Add file checking for size and type

--- client/src/pages/User/EditPhoto.jsx
+++ client/src/pages/User/EditPhoto.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import { Button, Image, Form } from 'semantic-ui-react'
+import { Button, Image, Form, Message } from 'semantic-ui-react'
 
 import http from "../../api/http";
 import AvatarCropper from './AvatarCropper'
@@ -21,29 +21,48 @@ import './User.css';
 const EditPhoto = (props) => {
     const [ uploadedFile, setUploadedFile ] = useState(null)
     const [ loading, setLoading ] = useState(false)
-    const [ isDisabled, setIsDisabled ] = useState(true)
+    // const [ isDisabled, setIsDisabled ] = useState(true)
     const [ cropWindow,  showCropWindow] = useState(false)
     const [ newAvatar, setNewAvatar ] = useState(null)
+    const [ error, setError ] = useState(null)
+    const [ success, setSuccess ] = useState(null)
+    // const [ message, setMessage ] = useState(null)
     
     const isFileValid = (file) => {
+        if (!file) return false
+
         const validExtentions = ['image/jpg', 'image/png', 'image/jpeg']
-        const maxSize = 15000000
-        return validExtentions.includes(file.type) && file.size <= maxSize
+        if (!validExtentions.includes(file.type)) {
+            setError(`Invalid image type. Allowed: ${validExtentions.join(', ')}`)
+            return false
+        }
+        const maxSize = 15728640
+        if (maxSize < file.size) {
+            setError('Invalid image size. Allowed < 15 mb')
+            return false
+        }
+
+        return true 
     }
     
     // event: React.ChangeEvent<HTMLInputElement>
     const singleFileChangedHandler = (event) => {
+        setSuccess(null)
         
         const reader = new FileReader();
         const file = event.target.files[0];
         // todo test if non-file choosed
         const valid = isFileValid(file)
-
+        if (!valid) return 
+        setLoading(true)
+        
+    
         reader.onloadend = () => {
             setUploadedFile(file)
             setNewAvatar(reader.result)
-            setIsDisabled(!valid)
-            showCropWindow(true)
+            setError(null)
+            showCropWindow(true) 
+            setLoading(false)
         }
         reader.readAsDataURL(file)
     };
@@ -51,6 +70,16 @@ const EditPhoto = (props) => {
     const cropImage = (file) => {
         setUploadedFile(file)
         showCropWindow(false)
+    }
+
+    const getMessage = () => {
+        if (error) {
+            return <Message negative header={error} />
+             
+        }
+        if (success) {
+            return <Message success header={success} />
+        }
     }
 
     const singleFileUploadHandler = () => {
@@ -64,18 +93,21 @@ const EditPhoto = (props) => {
             }
             
             data.append('avatarImage', uploadedFile, uploadedFile.name);
-            http.post('/api/aws/upload-avatar', data, requestOptions)
+            http.get('/api/aws/upload-avatar', data, requestOptions)
                 .then( response => {
                     setLoading(false)
+                    setError(null)
+                    setSuccess('Avatar has successfully updated')
+                    
                     const avatar = response.data.updatedUser.imageUrl
-                    console.log('ok')
                     props.setAvatar(avatar)
+                    console.log(response.data)
                         // todo test non-200  
                 })
                 .catch( error => {
                     setLoading(false)
-                    setIsDisabled(true)
-                    console.log('error')
+                    setError('Something went wrong. Please, try again!')
+                    setSuccess(null)
                 })
         }
     }
@@ -90,10 +122,13 @@ const EditPhoto = (props) => {
                         <div className="imgPreview">
                             <Image src={newAvatar || props.avatar} />  
                         </div>
+                        <div>
+                            {getMessage()}
+                        </div>
                         {cropWindow && <AvatarCropper avatar={newAvatar} setNewAvatar={setNewAvatar} cropImage={cropImage}/>}
                         <input type="file" id="file" onChange={singleFileChangedHandler} />
                         <label htmlFor="file">Upload image</label>
-                        <Button className="save-btn" onClick={singleFileUploadHandler} disabled={isDisabled} color="red">Save</Button>
+                        <Button className="save-btn" onClick={singleFileUploadHandler} disabled={!!error} color="red">Save</Button>
                     </div>
                 </div>
             </Form>
